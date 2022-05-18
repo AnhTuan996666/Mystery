0d800bca64816e73986ec972df29cff2433e111a
VietIS Cuong <cuongvv@vietis.com.vn>
2022-05-18 09:14:59 +0700

    payment-form

---
+++ block/progress_bar.php
@@ -0,0 +1,7 @@
+<div class="progressbar">
+    <ul class="progressbar-list">
+        <li class="progressbar-list__item success">Thông tin nhận hàng</li>
+        <li class="progressbar-list__item active"><span class="success"></span>Phương thức thanh toán</li>
+        <li class="progressbar-list__item"><span></span>Đặt hàng thành công</li>
+    </ul>
+</div>
